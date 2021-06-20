466357ef8e926b9252bf0265ce501e5474a0881c
Chester Mendoza <ronchester.mendoza@gmail.com>
2021-06-20 21:12:35 +0800

    removes logs

--- src/containers/Stats/index.tsx
+++ src/containers/Stats/index.tsx
@@ -2,8 +2,6 @@ import { Box, Flex, Progress, Stack, Text } from '@chakra-ui/react'
 import { StatsProps } from './types'
 
 const Stats: React.FC<StatsProps> = ({ stats }) => {
-  console.log(stats)
-
   return (
     <Stack spacing={4}>
       {stats?.map((item, key) => (
@@ -15,7 +13,12 @@ const Stats: React.FC<StatsProps> = ({ stats }) => {
             <Text as='span' w={{ sm: '20%', md: '10%' }}>
               {item?.base_stat}
             </Text>
-            <Progress flex='1' value={item?.base_stat} borderRadius='2xl' />
+            <Progress
+              flex='1'
+              value={item?.base_stat}
+              max={255}
+              borderRadius='2xl'
+            />
           </Flex>
         </Flex>
       ))}
